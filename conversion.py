67aff9150f682bb6496f225f3e9f8ab8af83531b
rileyk94 <rileyk94@gmail.com>
2025-05-24 17:54:14 -0500

    feat: add user selected file

--- conversion.py
+++ conversion.py
@@ -7,6 +7,12 @@ for fic in fic_list:
     print(f"{file_num}. {fic}")
     file_num +=1
 
-# file_selected = false
-# while file_selected == false:
-    
+file_selected = False
+while file_selected == False:
+    choose = int(input("Please select the file number of the file you wish to be converted: "))
+    if choose > 0 and choose <= len(fic_list):
+        file = fic_list[choose-1]
+        file_selected = True
+    else:
+        print("please try again")
+print(file)
